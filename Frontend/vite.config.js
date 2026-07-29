08bcfa8ff671898ed07427d8353289953a6270c0
Saulo Rangel <saulorangel10@hotmail.com>
2026-07-29 07:21:04 -0300

    Configura deploy do GitHub Pages

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/rotacddcampos/",
   server: {
-    port: 5173
-  }
-})
+    port: 5173,
+  },
+});
